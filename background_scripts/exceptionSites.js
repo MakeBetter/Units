becee844468f79726f08fe8cddcd45cc097a960d
Himanshu Pokhariya <himanshu2004@gmail.com>
2013-03-29 18:37:33 +0530

    [Fix] Fix issue introduced by previous commit

--- background_scripts/exceptionSites.js
+++ background_scripts/exceptionSites.js
@@ -2,7 +2,7 @@
 The data in this file specifies sites where the extension should not run/run in partial mode.
 Patterns for matching urls can be specified using either the 'url pattern' format allowing '@' and '*' wildcards,
 or regexp objects the aforementioned patterns don't suffice. Details about the 'url pattern' format can be found
-in the urlData.js file.
+in the unitsData.js file.
 */
 
 var  exceptionSites = {
