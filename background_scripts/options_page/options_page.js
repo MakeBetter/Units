c8e6b38b4e8a27818cf19af792e61de49863b05e
Tanushree Jindal <tanushree.jindal@gmail.com>
2013-05-01 12:14:46 +0530

    [Feature] Implement a basic functional 'Reset Settings' feature

--- background_scripts/options_page/options_page.js
+++ background_scripts/options_page/options_page.js
@@ -5,8 +5,8 @@ var backgroundPageWindow = chrome.extension.getBackgroundPage(),
     "use strict";
 
     var settingsTextArea = document.getElementById("userSettingsJSON"),
-        saveSettingsButton = document.getElementById("save-settings");
-
+        saveSettingsButton = document.getElementById("save-settings"),
+        resetSettingsButton = document.getElementById("reset-settings");
 
     var populateUserOptions = function() {
         var settings = mod_settings.getAllSettings();
@@ -17,22 +17,31 @@ var backgroundPageWindow = chrome.extension.getBackgroundPage(),
         settingsTextArea.value = settings;
     };
 
-
-    var eh_saveSettings = function() {
-        saveSettings(settingsTextArea.value);
-    };
-
     var saveSettings = function(settingsText) {
         var settingsObj = JSON.parse(settingsText);
         helper.destringifyJsonUnsupportedTypes_inSettings(settingsObj);
 
         mod_settings.setUserSettings(settingsObj);
 
-        console.log("settings saved!"); // till there is no user feedback, let's keep this.
+        console.log("settings saved."); // till there is no user feedback, let's keep this.
+    };
+
+
+    var eh_saveSettings = function() {
+        saveSettings(settingsTextArea.value);
+    };
+
+    var resetSettings = function() {
+      if (confirm("Sure you want to reset the settings ?")) {
+          delete localStorage.userSettings;
+          populateUserOptions();
+
+          console.log("settings reset."); // till there is no user feedback, let's keep this.
+      }
     };
 
     populateUserOptions();
     saveSettingsButton.addEventListener("click", eh_saveSettings);
-
+    resetSettingsButton.addEventListener("click", resetSettings);
 
 })(_u.helper, _u.mod_settings);
