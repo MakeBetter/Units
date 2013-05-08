092a6c88f5f20d03dfa6834b2d45b441da87a4ac
Tanushree Jindal <tanushree.jindal@gmail.com>
2013-05-08 13:55:42 +0530

    [Feature] In the help UI, display shortcuts under two sections, 'CUs based' and 'General'
    TODO:
    - Some basic HTML and CSS for the help sections.
    - Use a better word than 'CU' to indicate CUs to the user.

--- content_scripts/mod_help.js
+++ content_scripts/mod_help.js
@@ -1,4 +1,6 @@
-
+/***
+ * Module for displaying the "help" modal UI
+ */
 _u.mod_help = (function($, mod_core, mod_contentHelper, CONSTS) {
     "use strict";
 
@@ -14,7 +16,31 @@ _u.mod_help = (function($, mod_core, mod_contentHelper, CONSTS) {
     var $helpContainer,
         class_addedByUnitsProj = CONSTS.class_addedByUnitsProj,
         $topLevelContainer = mod_core.$topLevelContainer,
-        suppressEvent = mod_contentHelper.suppressEvent;
+        suppressEvent = mod_contentHelper.suppressEvent,
+        helpModalDialogHtml =
+            '<div id = "UnitsProj-help-container">' +
+                '<div class = "UnitsProj-modal-backdrop">' +
+                '</div>' +
+
+                '<div id= "help" class = "UnitsProj-modal">' +
+                '<div class= "UnitsProj-modal-header">' +
+                '<h1 class="UnitsProj-modal-title">Units Shortcuts</h1>' +
+                '<button class= close>×</button>' +
+                '</div>' +
+
+                '<div class = "UnitsProj-modal-body">' +
+                '</div>' +
+
+                '</div>' +
+
+                '</div>',
+
+
+        shortcutsSectionHtml =
+            '<div class = "section">' +
+                '<span class= "section-title"></span>' +
+                '<table>' + '</table>' +
+            '</div>';
 
     function setup(settings) {
         setupHelpUI(settings);
@@ -35,92 +61,87 @@ _u.mod_help = (function($, mod_core, mod_contentHelper, CONSTS) {
     }
 
     function setupHelpUI(settings) {
-
-        var browserShortcuts = settings.browserShortcuts,
-            generalShortcuts = settings.generalShortcuts,
-            CUsShortcuts = settings.CUsShortcuts,
-            expandedUrlData = settings.expandedUrlData;
-
-        var helpModalDialogHtml =
-            '<div id = "UnitsProj-help-container">' +
-                '<div class = "UnitsProj-modal-backdrop">' +
-                '</div>' +
-
-                '<div id= "help" class = "UnitsProj-modal">' +
-                '<div class= "UnitsProj-modal-header">' +
-                '<h1 class="UnitsProj-modal-title">Units Shortcuts</h1>' +
-                '<button class= close>×</button>' +
-                '</div>' +
-
-                '<div class = "UnitsProj-modal-body">' +
-                '</div>' +
-
-                '</div>' +
-
-                '</div>';
-
-
-        var shortcutsSectionHtml =
-            '<div class = "section">' +
-                '<span class= "section-title"></span>' +
-                '<table>' + '</table>' +
-                '</div>';
-
         $helpContainer = $(helpModalDialogHtml);
+        var $help = $helpContainer.find("#help"),
+        // shortcuts are displayed in two sections: CUs Shortcuts, and General Shortcuts.
+            $CUsShortcutsSection = getCUsShortcutsSection(settings),
+            $generalShortcutsSection = getGeneralShortcutsSection(settings);
 
-        var $help = $helpContainer.find("#help");
-
-
-        // shortcuts are displayed under three different heads: CUs based, page based, and general browser based.
-
-        var CUs_MUs = expandedUrlData && expandedUrlData.CUs_MUs,
-            CUs_actions = expandedUrlData && expandedUrlData.CUs_actions,
-            CUs_allShortcuts = $.extend({}, CUs_MUs, CUs_actions, CUsShortcuts);
-
-        var page_MUs = expandedUrlData && expandedUrlData.page_MUs,
-            page_actions = expandedUrlData && expandedUrlData.page_actions,
-            page_allShortcuts = $.extend({}, page_MUs, page_actions);
-
-        var allGeneralShortcuts = $.extend({},browserShortcuts, generalShortcuts);
-        // TODO: the generalShortcuts contains quite a few CUs based shortcuts. Make changes to the data to be able to
-        // identify these CUs based shortcuts, and then show them as part of the CUs shortcuts.
-
-        var appendSectionShortcuts = function(shortcuts, sectionTitle) {
-            if (!shortcuts || !sectionTitle) {
-                return;
-            }
-
-            var $section = $(shortcutsSectionHtml),
-                $shortcutsTable = $section.find("table"),
-                hasAtLeastOneShortcut = false;
-
-            $.each(shortcuts, function(key, value) {
-                if (value.kbdShortcuts) {
-                    $("<tr></tr>")
-                        .appendTo($shortcutsTable)
-                        .append($("<td></td>").text(value.kbdShortcuts.toString().replace(",", ", ")))
-                        .append($("<td></td>").text(value.miniDescr || key));
-
-                    hasAtLeastOneShortcut = true;
-                }
-            });
-
-            if (hasAtLeastOneShortcut) {
-                $section.find(".section-title").text(sectionTitle);
-                $help.find(".UnitsProj-modal-body").append($section);
-            }
-
-        };
-
-        appendSectionShortcuts(CUs_allShortcuts, "CUs Shortcuts");
-        appendSectionShortcuts(page_allShortcuts, "Page Shortcuts");
-        appendSectionShortcuts(allGeneralShortcuts, "General Shortcuts");
+        $help.find(".UnitsProj-modal-body").append($CUsShortcutsSection, $generalShortcutsSection);
 
         $helpContainer
             .addClass(class_addedByUnitsProj)
             .hide()
             .appendTo($topLevelContainer);
     }
+
+    function getCUsShortcutsSection(settings) {
+        var CUsShortcuts_Default = settings.CUsShortcuts,
+            expandedUrlData = settings.expandedUrlData,
+            CUs_MUs = expandedUrlData && expandedUrlData.CUs_MUs,
+            CUs_actions = expandedUrlData && expandedUrlData.CUs_actions,
+            CUsShortcuts_URLBased = $.extend({}, CUs_MUs, CUs_actions);
+
+        var $section = $(shortcutsSectionHtml);
+        $section
+            .attr("id", "CUs-shortcuts")
+            .find(".section-title").text("CUs based");
+
+        var $shortcutsTable = $section.find("table");
+
+        renderShortcutsInSectionTable(CUsShortcuts_Default, $shortcutsTable);
+        renderShortcutsInSectionTable(CUsShortcuts_URLBased, $shortcutsTable);
+
+        return $section;
+
+    }
+
+    function getGeneralShortcutsSection(settings) {
+        var browserShortcuts = settings.browserShortcuts,
+            generalShortcuts = settings.generalShortcuts,
+            expandedUrlData = settings.expandedUrlData;
+
+        var page_MUs = expandedUrlData && expandedUrlData.page_MUs,
+            page_actions = expandedUrlData && expandedUrlData.page_actions,
+            page_allShortcuts = $.extend({}, page_MUs, page_actions);
+
+        var $section = $(shortcutsSectionHtml);
+        $section
+            .attr("id", "general-shortcuts")
+            .find(".section-title").text("General");
+
+        var $shortcutsTable = $section.find("table");
+
+        renderShortcutsInSectionTable(generalShortcuts, $shortcutsTable);
+        renderShortcutsInSectionTable(page_allShortcuts, $shortcutsTable);
+        renderShortcutsInSectionTable(browserShortcuts, $shortcutsTable);
+
+        return $section;
+    }
+
+    function renderShortcutsInSectionTable(shortcutsObj, $shortcutsTable) {
+        if (!shortcutsObj || !$shortcutsTable) {
+            return;
+        }
+
+        var hasAtLeastOneShortcut = false;
+
+        $.each(shortcutsObj, function(key, value) {
+            if (value.kbdShortcuts) {
+                $("<tr></tr>")
+                    .appendTo($shortcutsTable)
+                    .append($("<td></td>").text(value.kbdShortcuts.toString().replace(",", ", ")))
+                    .append($("<td></td>").text(value.miniDescr || key));
+
+                hasAtLeastOneShortcut = true;
+            }
+        });
+
+        if (hasAtLeastOneShortcut) {
+          // TODO: show a marker to indicate end of this sub-section
+        }
+    }
+
 
     function showHelp() {
         $helpContainer.show();
