33a78987f357b256e3dfce61e99d8d11966bd646
Himanshu Pokhariya <himanshu2004@gmail.com>
2013-06-24 03:44:50 +0530

    [Feature] In-progress: select link feature

--- content_scripts/mod_main.js
+++ content_scripts/mod_main.js
@@ -4,7 +4,7 @@
  * main module (mod_main.js) This is the main module which runs the extension by using the other modules
  */
 (function($, mod_domEvents, mod_basicPageUtils, mod_CUsMgr, mod_urlSpecificShortcuts, mod_mutationObserver,
-          mod_keyboardLib, mod_filterCUs, mod_zenMode, mod_help, mod_quickSearchSelectedText, mod_selectFocusables,
+          mod_keyboardLib, mod_selectLink, mod_filterCUs, mod_zenMode, mod_help, mod_quickSearchSelectedText, mod_selectFocusables,
           mod_chromeAltHack, mod_contentHelper, mod_commonHelper, mod_context) {
     "use strict";
 
@@ -33,7 +33,7 @@
         // opposite order
         modulesToSetup = [mod_domEvents, mod_keyboardLib, mod_context, mod_chromeAltHack,
             // modules which define keyboard shortcuts are listed next, in order of priority
-            mod_quickSearchSelectedText, mod_help, mod_selectFocusables, mod_basicPageUtils, mod_filterCUs, mod_zenMode,
+            mod_quickSearchSelectedText, mod_help, mod_selectFocusables, mod_basicPageUtils, mod_selectLink, mod_filterCUs, mod_zenMode,
             mod_urlSpecificShortcuts, mod_CUsMgr];
 
     /*-- Module implementation --*/
@@ -75,7 +75,7 @@
 
         mod_mutationObserver.disable(true);
 
-        $topLevelContainer.empty().remove();
+        $topLevelContainer.remove();
 
 
         document.activeElement.blur(); // focus the body.
@@ -136,14 +136,12 @@
                 // do this before setting up other modules, so that it gets priority over shortcuts setup in them
                 mod_keyboardLib.bind(generalShortcuts.toggleExtension.kbdShortcuts, toggleExtensionTemporarily);
 
-
                 for (var i = 0; i < modulesToSetup.length; i++) {
                     var module = modulesToSetup[i];
                     module && module.setup && module.setup(settings);
                 }
 
                 mod_mutationObserver.enable();
-
             }
         );
     }
@@ -245,5 +243,5 @@
     //return thisModule; // not required for main module
 
 })(jQuery, _u.mod_domEvents, _u.mod_basicPageUtils, _u.mod_CUsMgr, _u.mod_urlSpecificShortcuts, _u.mod_mutationObserver,
-        _u.mod_keyboardLib, _u.mod_filterCUs, _u.mod_zenMode, _u.mod_help, _u.mod_quickSearchSelectedText, _u.mod_selectFocusables,
+        _u.mod_keyboardLib, _u.mod_selectLink, _u.mod_filterCUs, _u.mod_zenMode, _u.mod_help, _u.mod_quickSearchSelectedText, _u.mod_selectFocusables,
         _u.mod_chromeAltHack, _u.mod_contentHelper, _u.mod_commonHelper, _u.mod_context);
