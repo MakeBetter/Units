MakeBetter/Units

commit 2cef6b1f3387c68e6e08b692137efffb8b537f82

[Feature] In-progress Basic Options UI. Allow editing general keyboard shortcuts by typing them out.

background_scripts/options_page/mod_basicOptions.js
@@ -1,8 +1,7 @@
 // JSHint Config
-/* global mod_optionsHelper, _u */
+/* global mod_optionsHelper, _u, Mousetrap*/
 /* exported mod_basicOptions */
 
-// A template for modules in this project
 var mod_basicOptions = (function(mod_commonHelper, mod_settings, mod_optionsHelper) {
     "use strict";
 
@@ -12,16 +11,24 @@ var mod_basicOptions = (function(mod_commonHelper, mod_settings, mod_optionsHelp
         setup: setup
     };
 
-    var basicOptions = document.getElementById("basic-options"),
-        editingHelpMessage_className = "input-help-message";
+    var class_editingHelpMessage = "input-help-message",
+        class_kbdShortcut = "kbdshortcut",
+
+        settingType_miscSettings = "misc-settings",
+        settingType_generalShortcuts = "general-shortcuts";
 
 
     function setup() {
-        // Basic Options handlers
-        basicOptions.querySelector("#misc-settings").addEventListener("change", saveOptions_misc);
-        basicOptions.querySelector("#misc-settings").addEventListener("input", showMessage_editingHelp);
+        // Event handlers
+        document.getElementById("misc-settings").addEventListener("change", saveOptions_miscSettings);
+        document.getElementById("general-keyboard-shortcuts").addEventListener("focusout", saveOptions_generalShortcuts);
 
-        document.addEventListener("focusout", hideMessage_editingHelp);
+//        document.getElementById("basic-options").addEventListener("input", showMessage_editingHelp);
+//        document.addEventListener("focusout", hideMessage_editingHelp);
+
+        document.getElementById("general-keyboard-shortcuts").addEventListener("focusin", showMessage_startKbdShortcutInput);
+        document.getElementById("general-keyboard-shortcuts").addEventListener("keydown", captureKeyboardShortcut);
+
     }
 
     function _render(settings) {
@@ -35,13 +42,12 @@ var mod_basicOptions = (function(mod_commonHelper, mod_settings, mod_optionsHelp
 
         var tbody, tr, settingValue, innerHtml, key;
 
-        // Populate miscSettings
         var basicMiscSettings = {
             selectCUOnLoad: "Select Content Unit on page on load",
             keepSelectedCUCentered: "Keep selected CU centered on page",
             enhanceActiveElementVisibility: "Enhance the active element style",
             pageScrollDelta: "Page scroll delta"
-        }
+        };
 
         tbody = document.createElement("tbody");
 
@@ -76,6 +82,7 @@ var mod_basicOptions = (function(mod_commonHelper, mod_settings, mod_optionsHelp
 
         for (key in generalShortcuts) {
             shortcut = generalShortcuts[key];
+
             tr = document.createElement('tr');
             tr.setAttribute("id", key);
 
@@ -86,15 +93,15 @@ var mod_basicOptions = (function(mod_commonHelper, mod_settings, mod_optionsHelp
             kbdShortcuts = shortcut.kbdShortcuts;
             for (var i = 0; i < kbdShortcuts.length; i++) {
                 kbdShortcut = kbdShortcuts[i];
-                kbdShortcutsHtml += "<input type='text' value='" + kbdShortcut + "' />";
+                kbdShortcutsHtml += "<input type='text' class= '"+ class_kbdShortcut + "' value='" + kbdShortcut + "' />";
             }
 
             innerHtml += "<td>" + kbdShortcutsHtml + "</td>";
 
             tr.innerHTML = innerHtml;
             tbody.appendChild(tr);
-
         }
+
         generalShortcutsContainer.appendChild(tbody);
 
     }
@@ -103,45 +110,83 @@ var mod_basicOptions = (function(mod_commonHelper, mod_settings, mod_optionsHelp
         mod_settings.getUserSettings(_render);
     }
 
-    function saveOptions_misc(event) {
+    function saveOptions(event, settingType) {
+
+        if (!settingType) {
+            return;
+        }
+
         var target = event.target,
-            tagName = target.tagName.toLowerCase(),
-            type = target.type && target.type.toLowerCase(),
-            parentRow = target.parentElement && target.parentElement.parentElement,
+            targetTagName = target.tagName.toLowerCase(),
+            parentRow = target.parentElement && target.parentElement.parentElement;
+
+        if (targetTagName !== "input" || !parentRow || parentRow.tagName.toLowerCase() !== "tr") {
+            return;
+        }
+
+        var targetInputType = target.type && target.type.toLowerCase(),
             settings = mod_settings.getUserSettings(null, {getBasic: true}),
-            miscSettings = settings.miscSettings,
-            settingKey;
+            settingKey, settingValue;
 
-        if (parentRow.tagName.toLowerCase() === "tr") {
-            settingKey = parentRow.id;
+        if (targetInputType == "checkbox") {
+            settingValue = target.checked;
+        }
+        else {
+            settingValue = target.value;
         }
 
-        if (tagName === "input") {
-            if (type == "checkbox") {
-                miscSettings[settingKey] = target.checked;
-            }
-            else {
-                miscSettings[settingKey] = target.value;
+        settingKey = parentRow.id;
+
+        if (settingType === settingType_miscSettings) {
+            var miscSettings = settings.miscSettings;
+            miscSettings[settingKey] = settingValue;
+        }
+        else if (settingType === settingType_generalShortcuts) {
+            if (!settingValue) {
+                render(); // so that the textbox value is reset
+                return;
             }
 
-            saveOptions(settings, "Option saved");
+            var generalShortcuts = settings.generalShortcuts;
+            if (generalShortcuts[settingKey]) {
+                generalShortcuts[settingKey].kbdShortcuts = getKeyboardShortcutsForRow(parentRow); 
+            }
         }
+
+        mod_optionsHelper.saveOptions(settings, "Option saved", render);
+    }
+
+    function getKeyboardShortcutsForRow(row) {
+        var shortcutInputs = row.querySelectorAll("." + class_kbdShortcut),
+            shortcuts = [];
+
+        for (var i = 0; i < shortcutInputs.length; i++) {
+            var shortcut = shortcutInputs[i];
+            shortcuts.push(shortcut.value);
+        }
+
+        return shortcuts;
+    }
+
+    function saveOptions_miscSettings(event) {
+        saveOptions(event, settingType_miscSettings);
     }
 
     function saveOptions_generalShortcuts(event) {
-        var target = event.target;
+        saveOptions(event, settingType_generalShortcuts);
     }
+
 
     function showMessage_editingHelp(event) {
         var target = event.target,
             messageElement = document.createElement("span");
 
-        messageElement.classList.add(editingHelpMessage_className);
+        messageElement.classList.add(class_editingHelpMessage);
         messageElement.textContent = "Press enter or click outside the textbox to save.";
 
         var parent = target.parentElement;
 
-        if (!parent.querySelectorAll("." + editingHelpMessage_className).length) {
+        if (!parent.querySelectorAll("." + class_editingHelpMessage).length) {
             parent.appendChild(messageElement);
         }
     }
@@ -150,13 +195,45 @@ var mod_basicOptions = (function(mod_commonHelper, mod_settings, mod_optionsHelp
         var target = event.target,
             messageElement = target.nextElementSibling;
 
-        if (messageElement.classList.contains(editingHelpMessage_className)) {
+        if (messageElement && messageElement.classList.contains(class_editingHelpMessage)) {
             messageElement.remove();
         }
     }
 
-    function saveOptions(settings, successMessage) {
-        mod_optionsHelper.saveOptions(settings, successMessage, render);
+    function showMessage_startKbdShortcutInput(event) {
+        var target = event.target;
+        target.value = "";
+        target.setAttribute("placeholder", "Type a shortcut");
+    }
+
+    function captureKeyboardShortcut(event) {
+        var target = event.target,
+            targetValue = target.value,
+            modifiers = Mousetrap.eventModifiers(event),
+            characterTyped = Mousetrap.characterFromEvent(event);
+
+        // If the character typed is one of the modifiers, then disregard the input
+        if (modifiers.indexOf(characterTyped) > 0 || !characterTyped) {
+            return;
+        }
+
+        // For sequence shortcuts. Append the current typed shortcut to existing value
+        if (targetValue) {
+            targetValue += " ";
+        }
+
+
+        for (var i = 0; i < modifiers.length; i++) {
+            var modifier = modifiers[i];
+            targetValue += modifier + "+";
+        }
+
+        targetValue += characterTyped;
+
+        target.value = targetValue;
+
+        event.preventDefault();
+
     }
 
     return thisModule;

background_scripts/options_page/options_page.html
@@ -71,6 +71,7 @@
             <div id = advanced-options-help style="display: none"></div>
         </div>
     </div>
+    <script src="../../vendor/mousetrap-modified.js"></script>
     <script src="mod_optionsHelper.js"></script> <!--include first.-->
     <script src="mod_advancedOptions.js"></script>
     <script src="mod_basicOptions.js"></script>

vendor/mousetrap-modified.js
@@ -24,9 +24,11 @@
 to the same keyboard shortcut execute in the order in which they were bound (This required specific
 changes for "sequence" type shortcuts)
 
-5) Removed all instances of `_directMap`, `unbind`, `trigger` which are not needed and will no longer
+5. Removed all instances of `_directMap`, `unbind`, `trigger` which are not needed and will no longer
 work as expected
 
+6. Added methods _characterFromEvent and _eventModifiers to the public interface of the module. These are being used by
+mod_basicOptions.
  ----------------------------------------------------------------------------------
  */
 
@@ -995,7 +997,11 @@ work as expected
         /**
          * exposes _handleKey publicly so it can be overwritten by extensions
          */
-        handleKey: _handleKey
+        handleKey: _handleKey,
+
+        characterFromEvent: _characterFromEvent, // [Modification for UnitsProj] Required by mod_basicOptions
+        eventModifiers: _eventModifiers // [Modification for UnitsProj] Required by mod_basicOptions
+
     };
 
     // expose mousetrap to the global object

commit 74d3be86473a48a1632fbd7e8fe090aa6e210e2d

[Fix] Fix for issue introduced in recent commit
Commit: https://github.com/MakeBetter/Units/commit/2cef6b1f3387c68e6e08b692137efffb8b537f82

background_scripts/options_page/mod_basicOptions.js
@@ -213,7 +213,7 @@ var mod_basicOptions = (function(mod_commonHelper, mod_settings, mod_optionsHelp
             characterTyped = Mousetrap.characterFromEvent(event);
 
         // If the character typed is one of the modifiers, then disregard the input
-        if (modifiers.indexOf(characterTyped) > 0 || !characterTyped) {
+        if (!characterTyped || modifiers.indexOf(characterTyped) > -1) {
             return;
         }
 