b517a6be273a01e12d2e09725030b4c0b9bec1ef
Himanshu Pokhariya <himanshu2004@gmail.com>
2013-07-21 01:27:26 +0530

    [Feature] Improved search-as-you-type for findAnyLink feature

--- content_scripts/mod_selectLink.js
+++ content_scripts/mod_selectLink.js
@@ -10,13 +10,15 @@ _u.mod_selectLink = (function($, mod_domEvents, mod_contentHelper, mod_basicPage
 
 
     var $document = $(document),
-        timeout_typing,
         class_addedByUnitsProj = CONSTS.class_addedByUnitsProj,
         suppressEvent = mod_contentHelper.suppressEvent,
         matchingLink_class = 'UnitsProj-matchingLink',
         elementStyledAsActive,
         $empty = $(),   // saved reference
-        $matching = $empty;
+        $matching = $empty,
+
+        timeout_findMatchingLinks,
+        maxDelay_findMatchingLinks = 333;
 
     var $textBox =  $('<input id = "UnitsProj-selectLink-textBox" type = "text">')
         .addClass("UnitsProj-reset-text-input")
@@ -52,11 +54,18 @@ _u.mod_selectLink = (function($, mod_domEvents, mod_contentHelper, mod_basicPage
         mod_keyboardLib.bind(generalShortcuts.openSelectedLinkInNewTab.kbdShortcuts, openSelectedLink_newTab, {selectLinkUIActive: true}, true);
     }
 
-    function onInput() {
-        // to allow search-as-you-type, while not executing the filtering related code till there is a brief pause in the typing
-        clearTimeout(timeout_typing); // clears timeout if it is set
-        timeout_typing = setTimeout (findMatchingLinks, 300);
+    function reset() {
+        timeout_findMatchingLinks = false;
     }
+
+    // to allow search-as-you-type, while executing the findMatchingLinks code only periodically even if the user
+    // keeps typing continuously 
+    function onInput() {
+        // compare explicitly with false, which is how we reset it
+        if (timeout_findMatchingLinks === false) {
+            timeout_findMatchingLinks = setTimeout(findMatchingLinks, maxDelay_findMatchingLinks);
+        }
+    } 
 
     function selectNext() {
         select('n');
@@ -102,6 +111,9 @@ _u.mod_selectLink = (function($, mod_domEvents, mod_contentHelper, mod_basicPage
     }
 
     function findMatchingLinks() {
+        clearTimeout(timeout_findMatchingLinks);
+        timeout_findMatchingLinks = false;    // reset
+       
         $matching.removeClass(matchingLink_class);
         removeActiveElementStyling();
 
@@ -155,7 +167,8 @@ _u.mod_selectLink = (function($, mod_domEvents, mod_contentHelper, mod_basicPage
 
     function closeUI() {
         var disabledByMe = mod_mutationObserver.disable();
-        clearTimeout(timeout_typing); // clears timeout if it is set
+        clearTimeout(timeout_findMatchingLinks);
+        timeout_findMatchingLinks = false;    // reset
 
         // blur, if not already blurred (the check exists to prevent infinite recursion)
         if (document.activeElement === $textBox[0])
