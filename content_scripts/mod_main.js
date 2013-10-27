ddf0b48167e2ef742430cef1005857305ceb7312
Himanshu Pokhariya <himanshu2004@gmail.com>
2013-10-28 05:06:04 +0530

    [Fix] Remove some unneeded code + add jQuery.min.map

--- content_scripts/mod_main.js
+++ content_scripts/mod_main.js
@@ -4,7 +4,7 @@
  * main module (mod_main.js) This is the main module which runs the extension by using the other modules
  */
 (function($, mod_domEvents, mod_easyScrollKeys, mod_basicPageUtils, mod_CUsMgr, mod_urlSpecificShortcuts, mod_mutationObserver,
-          mod_keyboardLib, mod_selectLink, mod_filterCUs, mod_zenMode, mod_help, mod_quickSearchSelectedText,
+          mod_keyboardLib, mod_selectLink, mod_filterCUs, mod_zenMode, mod_help,
           mod_chromeAltHack, mod_contentHelper, mod_commonHelper, mod_globals) {
     "use strict";
 
@@ -37,7 +37,7 @@
         // opposite order
         modulesToSetup = [mod_domEvents, mod_keyboardLib, mod_globals, mod_chromeAltHack,
             // modules which define keyboard shortcuts are listed next, in order of priority
-            mod_quickSearchSelectedText, mod_help, mod_selectLink, mod_easyScrollKeys, mod_basicPageUtils, mod_filterCUs, mod_zenMode,
+            mod_help, mod_selectLink, mod_easyScrollKeys, mod_basicPageUtils, mod_filterCUs, mod_zenMode,
             mod_urlSpecificShortcuts, mod_CUsMgr];
 
     /*-- Module implementation --*/
@@ -212,33 +212,33 @@
         }
     }
 
-    function onWindowMessage(event, targetOrigin) {
-        var data = event.data;
-//            isMessageOriginValid = (mod_commonHelper.getHostname(event.origin) === mod_commonHelper.getHostname(chrome.runtime.getURL("")));
-
-        if (!data/* ||  !isMessageOriginValid*/) {
-            return;
-        }
-
-        if (data.message === 'hideHelpUI') {
-            mod_help.hideHelp(); // hide the help iframe
-            window.focus(); // restore focus to the window.
-        }
-        else if (data.message === 'setHelpUIHeight') {
-            mod_help.positionHelpUI(data.height);
-        }
-        else if(data.message === 'doesPageHaveCUsSpecifier') {
-            var pageHasCUsSpecifier = mod_globals.pageHasCUsSpecifier;
-
-            var iframeHelp = document.getElementById("UnitsProj-iframe-help");
-            iframeHelp.contentWindow.postMessage({message: 'pageHasCUsSpecifier', value: pageHasCUsSpecifier}, '*');
-        }
-        return false;
-    }
+//    function onWindowMessage(event, targetOrigin) {
+//        var data = event.data;
+////            isMessageOriginValid = (mod_commonHelper.getHostname(event.origin) === mod_commonHelper.getHostname(chrome.runtime.getURL("")));
+//
+//        if (!data/* ||  !isMessageOriginValid*/) {
+//            return;
+//        }
+//
+//        if (data.message === 'hideHelpUI') {
+//            mod_help.hideHelp(); // hide the help iframe
+//            window.focus(); // restore focus to the window.
+//        }
+//        else if (data.message === 'setHelpUIHeight') {
+//            mod_help.positionHelpUI(data.height);
+//        }
+//        else if(data.message === 'doesPageHaveCUsSpecifier') {
+//            var pageHasCUsSpecifier = mod_globals.pageHasCUsSpecifier;
+//
+//            var iframeHelp = document.getElementById("UnitsProj-iframe-help");
+//            iframeHelp.contentWindow.postMessage({message: 'pageHasCUsSpecifier', value: pageHasCUsSpecifier}, '*');
+//        }
+//        return false;
+//    }
 
     // Used to communicate with the Help UI iframe. This event does not need to be bound using mod_domEvents because we
     // don't need to remove it when extension is disabled.
-    window.addEventListener('message', onWindowMessage, false);
+//    window.addEventListener('message', onWindowMessage, false);
 
     // don't need to wait till dom-ready. allows faster starting up of the extension's features
     // (in certain sites at least. e.g. guardian.co.uk)
@@ -256,5 +256,5 @@
     //return thisModule; // not required for main module
 
 })(jQuery, _u.mod_domEvents, _u.mod_easyScrollKeys, _u.mod_basicPageUtils, _u.mod_CUsMgr, _u.mod_urlSpecificShortcuts, _u.mod_mutationObserver,
-        _u.mod_keyboardLib, _u.mod_selectLink, _u.mod_filterCUs, _u.mod_zenMode, _u.mod_help, _u.mod_quickSearchSelectedText,
+        _u.mod_keyboardLib, _u.mod_selectLink, _u.mod_filterCUs, _u.mod_zenMode, _u.mod_help,
         _u.mod_chromeAltHack, _u.mod_contentHelper, _u.mod_commonHelper, _u.mod_globals);
